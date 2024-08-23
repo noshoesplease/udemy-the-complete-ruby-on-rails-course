ab54b684b02d6944fbed76baec7fe73266615d4f
Devin Custodio <devin.roy.custodio@gmail.com>
2024-08-23 07:50:24 -0400

    comparison_operators

---
+++ the_ruby_programming_language/comparison_operators.rb
@@ -0,0 +1,31 @@
+puts "10 == 9"
+puts 10 == 9
+puts
+
+puts "10 == 10"
+puts 10 == 10
+puts
+
+puts "10 == '10'.to_i"
+puts 10 == '10'.to_i
+puts
+
+puts "10 == '10'.to_f"
+puts 10 == '10'.to_f
+puts
+
+puts "10 != 9"
+puts 10 != 9
+puts
+
+puts "100 > 99"
+puts 100 > 99
+puts
+
+puts "100 > 101"
+puts 100 > 101
+puts
+
+puts "10.eql?(10.0)"
+puts 10.eql?(10.0) # compares types. evaluates to false
+puts
