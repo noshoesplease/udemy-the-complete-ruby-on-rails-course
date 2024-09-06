38ea83374d174fde930bd37e134b3d099f99fb33
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-06 12:01:45 -0400

    Validations

--- intro_to_rails/alpha_blog/app/models/article.rb
+++ intro_to_rails/alpha_blog/app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  # article = Article.new
+  # article.save // returns false and wont save
+  # if a validator is present and the
+  # title or description are not present
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
