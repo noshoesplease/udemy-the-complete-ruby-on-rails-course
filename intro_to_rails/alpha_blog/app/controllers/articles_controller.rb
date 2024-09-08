fb50357cce5c7e62790c674dfeac4119c19f4e11
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-08 09:08:35 -0400

    DRY (Don't Repeat Yourself) code - refactoring and partials
    DRY - Don't Repeat Yourself - code can be accomplished by refactoring or 'extracting away code redundancies'. We accomplished a significant amount of this by using a couple of private methods in our articles controller and using partials in our views. Clean code should always be a priority once features are built and working as we want so that our code is easy to read and easily maintainable. All changes we made to the application during the video can be found here:
    
    https://github.com/udemyrailscourse/alpha-blog-6/commit/b36fdad0dfb852d07c59b0c7b04e810cd13778e0

--- intro_to_rails/alpha_blog/app/controllers/articles_controller.rb
+++ intro_to_rails/alpha_blog/app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :load_article, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @articles = Article.all
   end
   def show
     # debugger
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -13,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     @article.author = "dev"
     if @article.save
       flash[:notice] = "Article was created successfully."
@@ -24,8 +25,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -34,13 +34,20 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was deleted successfully."
     redirect_to articles_path
+  end
+
+  private
+  def load_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
