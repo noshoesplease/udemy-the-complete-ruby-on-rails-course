5a44318dfeb064ee37143d32c70c736e718ae8b2
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-19 15:37:39 -0400

    rails db:migrate

--- intro_to_rails/alpha_blog/db/schema.rb
+++ intro_to_rails/alpha_blog/db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_05_133135) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_19_193702) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
