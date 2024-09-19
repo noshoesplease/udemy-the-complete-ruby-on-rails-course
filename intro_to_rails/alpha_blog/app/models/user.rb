bf54d1230da45e172e817286bd5cc55f37cdf3b4
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-19 16:24:50 -0400

    Create users

---
+++ intro_to_rails/alpha_blog/app/models/user.rb
@@ -0,0 +1,19 @@
+=begin
+
+association types for ActiveRecord entities
+  belongs_to
+  has_one
+has_many
+has_many :through
+has_one :through
+has_and_belongs_to_many
+
+=end
+
+class User < ApplicationRecord
+end
+
+
+__END__
+
+asdfsdafds
--- intro_to_rails/alpha_blog/db/schema.rb
+++ intro_to_rails/alpha_blog/db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_19_193702) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
