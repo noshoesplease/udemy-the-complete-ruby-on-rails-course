1283195f3edc82f76a531d0861131503c5f01f3a
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-20 20:09:56 -0400

    Show user and profile image

--- intro_to_rails/alpha_blog/app/controllers/users_controller.rb
+++ intro_to_rails/alpha_blog/app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     # used in the template if there are errors
     @user = User.new
--- intro_to_rails/alpha_blog/app/helpers/application_helper.rb
+++ intro_to_rails/alpha_blog/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravitar_for(user)
+    image_tag(
+      "https://media.licdn.com/dms/image/v2/D4E35AQHDBqO2gIGn6g/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1724785442368?e=1727470800&v=beta&t=X7n05LyqTX4Ll8X0Bm_m6k_-WnON3Ysy66dBCiGivkM",
+      alt: user.username,
+      class: "rounded mx-auto d-block",
+      style: "max-width: 400px; width: 100%; height: auto;"
+    )
+  end
 end
