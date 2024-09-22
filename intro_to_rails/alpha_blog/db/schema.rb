d29eb8c18cc55f546733d13554326c0a0bc529b7
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-22 07:57:10 -0400

    Add admin user functionality - intro

--- intro_to_rails/alpha_blog/db/schema.rb
+++ intro_to_rails/alpha_blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_20_105513) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_22_115502) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -26,5 +26,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_20_105513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 end
