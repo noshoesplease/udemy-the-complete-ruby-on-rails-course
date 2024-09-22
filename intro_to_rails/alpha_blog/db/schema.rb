de83271f004f2bb05f02bb5bbd9478066ffe3e72
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-22 09:11:00 -0400

    Category model and testing

--- intro_to_rails/alpha_blog/db/schema.rb
+++ intro_to_rails/alpha_blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_22_115502) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_22_130841) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_22_115502) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
