21c0b6271e09876524b5e3d7c95313ecea2fff3b
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-21 20:04:43 -0400

    Controller methods as helper methods

--- intro_to_rails/alpha_blog/app/controllers/application_controller.rb
+++ intro_to_rails/alpha_blog/app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # makes methods available in the views, as well as the controllers
+  # so we can remove it from the application_helper.rb
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    user_id = session[:user_id]
+    @current_user ||= User.find user_id if user_id
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
--- intro_to_rails/alpha_blog/app/controllers/articles_controller.rb
+++ intro_to_rails/alpha_blog/app/controllers/articles_controller.rb
@@ -16,8 +16,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.author = "dev"
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
