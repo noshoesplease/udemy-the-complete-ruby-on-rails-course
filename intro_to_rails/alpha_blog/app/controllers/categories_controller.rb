38f83f4ac6e1a2169ba59c0b95687d99e0dce2d0
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-22 11:05:50 -0400

    Create category and test

--- intro_to_rails/alpha_blog/app/controllers/categories_controller.rb
+++ intro_to_rails/alpha_blog/app/controllers/categories_controller.rb
@@ -3,8 +3,24 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:notice] = "Category was created successfully"
+      redirect_to @category
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
