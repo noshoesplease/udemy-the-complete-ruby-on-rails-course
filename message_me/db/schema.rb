71caf605ba380df7afab1378d5379ccb989e3542
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-26 14:53:33 -0400

    Build Message resource

--- message_me/db/schema.rb
+++ message_me/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_26_184034) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_26_184742) do
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
