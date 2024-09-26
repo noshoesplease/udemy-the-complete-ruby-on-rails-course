60e60e0ab7a4dce823b9bb2bdb0740a954a4bd70
Devin Custodio <devin.roy.custodio@gmail.com>
2024-09-26 18:41:38 -0400

    Add authentication system - create and destroy sessions

--- message_me/app/controllers/sessions_controller.rb
+++ message_me/app/controllers/sessions_controller.rb
@@ -3,20 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user = User.find_by email: params[:session][:email].downcase
-    # if user and user.authenticate(params[:session][:password])
-    #   session[:user_id] = user.id
-    #   flash[:notice] = "You have successfully logged in"
-    #   redirect_to user
-    # else
-    #   flash.now[:alert] = "There was something wrong with your login information"
-    #   render :new, status: :unprocessable_entity
-    # end
+    user = User.find_by username: params[:session][:username].downcase
+    if user and user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your login information"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    # session[:user_id] = nil
-    # flash[:notice] = "Logged out"
-    # redirect_to root_path
+    session[:user_id] = nil
+    flash[:success] = "Logged out"
+    redirect_to root_path
   end
 end
--- message_me/app/javascript/application.js
+++ message_me/app/javascript/application.js
@@ -1,8 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails";
 import "controllers"
 
 import * as jquery from "jquery";
 import "semantic-ui";
 
-$(".ui.dropdown").dropdown();
+$(document).on("turbo:load", function () {
+  $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
+});
