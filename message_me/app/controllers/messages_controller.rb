c2365ac9b62c4f16c08968a57bd1ede6418a585b
Devin Custodio <devin.roy.custodio@gmail.com>
2024-10-01 12:34:44 -0400

    Modify and broadcast messages

--- message_me/app/controllers/messages_controller.rb
+++ message_me/app/controllers/messages_controller.rb
@@ -4,6 +4,9 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
+      ActionCable.server.broadcast "chatroom_channel", render_message(message)
+      head :ok
+    else
       redirect_to root_path
     end
   end
@@ -11,5 +14,10 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def render_message(message)
+    # This will render the _message partial as HTML
+    ApplicationController.render(partial: "messages/message", locals: { message: message })
   end
 end
