5f722f0df6d1efd94e2cdf8bc08734d4dacbbbbf
Devin Custodio <devin.roy.custodio@gmail.com>
2024-10-04 16:30:44 -0400

    Update to use Turbo

--- finance_tracker/app/controllers/stocks_controller.rb
+++ finance_tracker/app/controllers/stocks_controller.rb
@@ -1,16 +1,35 @@
+# class StocksController < ApplicationController
+#   def search
+#     if params[:stock].present?
+#       @stock = Stock.new_lookup(params[:stock])
+#       if @stock
+#         render "users/my_portfolio"
+#       else
+#         flash[:alert] = "Please enter a valid symbol to search"
+#         redirect_to my_portfolio_path
+#       end
+#     else
+#       flash[:alert] = "Please enter a symbol to search"
+#       redirect_to my_portfolio_path
+#     end
+#   end
+# end
+
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-      @stock = Stock.new_lookup(params[:stock])
-      if @stock
-        render "users/my_portfolio"
-      else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+    stock_symbol = params[:stock].is_a?(Array) ? params[:stock].first : params[:stock]
+    @stock = Stock.new_lookup(stock_symbol)
+
+    if @stock
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("stock_results", partial: "users/result", locals: { stock: @stock }) }
+        format.html { render "users/my_portfolio" }
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("stock_results", partial: "users/result", locals: { stock: nil }) }
+        format.html { redirect_to my_portfolio_path, alert: "Stock not found" }
+      end
     end
   end
 end
