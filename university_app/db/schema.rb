8bd16ac8e6f0bde9278de567cbdf885b8ebfca81
Devin Custodio <devin.roy.custodio@gmail.com>
2024-10-29 18:55:03 -0400

    Add secure password

--- university_app/db/schema.rb
+++ university_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_164927) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_223833) do
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,5 +24,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_164927) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "email"
+    t.string "password_digest"
   end
 end
