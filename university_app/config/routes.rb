54f2f2cc63f54238d10290b952bbcc7db0f88c66
Devin Custodio <devin.roy.custodio@gmail.com>
2024-10-30 13:36:40 -0400

    Create and destroy sessions - add auth methods

--- university_app/config/routes.rb
+++ university_app/config/routes.rb
@@ -17,4 +17,8 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 
   resources :students, except: [ :destroy ]
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
