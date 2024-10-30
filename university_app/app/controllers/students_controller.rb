3e502cd3bbfa08ad72d7ed5d7eacb6f6b2272a93
Devin Custodio <devin.roy.custodio@gmail.com>
2024-10-29 20:23:19 -0400

    Update forms to accept passwords and modify

--- university_app/app/controllers/students_controller.rb
+++ university_app/app/controllers/students_controller.rb
@@ -37,6 +37,6 @@ class StudentsController < ApplicationController
 
   private
   def student_params
-    params.require(:student).permit(:name, :email)
+    params.require(:student).permit(:name, :email, :password, :password_confirmation)
   end
 end
